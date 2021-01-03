068583862be4f36b338bf9c3f30b269b3d21051f
Alexey <alexey.cherevuta@devit-team.com>
2021-01-03 08:48:00 +0000

    adding Debugbar barryvdh/laravel-debugbar

--- config/app.php
+++ config/app.php
@@ -38,6 +38,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
     ],
     'aliases' => [
         'App' => Illuminate\Support\Facades\App::class,
