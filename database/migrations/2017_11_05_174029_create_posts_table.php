a4f9994b85acbb6a42dc724f9e12e27c1ccc172d
Alexey <alexey.cherevuta@devit-team.com>
2021-01-21 11:57:57 +0000

    adding posts migration, model and controller

---
+++ database/migrations/2017_11_05_174029_create_posts_table.php
@@ -0,0 +1,20 @@
+<?php
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+class CreatePostsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->longText('content');
+            $table->timestamps();
+        });
+    }
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
