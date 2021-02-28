bd0f1a068c899f4910b5c46510c08c940d2d1275
Alexey <alexey.cherevuta@devit-team.com>
2021-02-28 12:43:31 +0000

    adding PostController ressource model with his route and view blog.index

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,4 +3,26 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 class PostController extends Controller
 {
+    public function index()
+    {
+        return view('blog.index');
+    }
+    public function create()
+    {
+    }
+    public function store(Request $request)
+    {
+    }
+    public function show($id)
+    {
+    }
+    public function edit($id)
+    {
+    }
+    public function update(Request $request, $id)
+    {
+    }
+    public function destroy($id)
+    {
+    }
 }
