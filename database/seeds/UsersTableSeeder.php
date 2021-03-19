bb63121909e8f889e62ec2aad995e090b1b01ed5
Alexey <alexey.cherevuta@devit-team.com>
2021-03-19 13:28:26 +0000

    Encrypt passwords in seeders...

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -5,26 +5,27 @@ class UsersTableSeeder extends Seeder
 {
     public function run()
     {
+        $pwd = 'zebulon';
         $users = [
             [
                 'name'     => 'cilia',
                 'email'    => 'cecilia@crtx.tech',
-                'password' => 'zebulon',
+                'password' => bcrypt($pwd),
             ],
             [
                 'name'     => 'Sp33dy',
                 'email'    => 'erwarkreutz@gmail.com',
-                'password' => 'zebulon',
+                'password' => bcrypt($pwd),
             ],
             [
                 'name'     => 'tr0ll',
                 'email'    => 'pichot.vincent@gmail.com',
-                'password' => 'zebulon',
+                'password' => bcrypt($pwd),
             ],
         ];
         foreach ($users as $user) {
             $newUser = User::create($user);
-            $this->command->info("[User #{$newUser->id}] User created with name: {$newUser->name} | email: {$newUser->email} | pwd: {$newUser->password}");
+            $this->command->info("[User #{$newUser->id}] User created with name: {$newUser->name} | email: {$newUser->email} | pwd: {$pwd}");
             $newUser->assignRole('admin');
             $this->command->info("[User #{$newUser->id}] User assigned to role admin");
         }
