7c98f7f8c634dd89fcc5b0e3e8f8b1118270a613
Alexey <alexey.cherevuta@devit-team.com>
2021-03-22 02:33:55 +0000

    Fixed table names for users' roles and permissions (from spatie/laravel-permission)

--- config/permission.php
+++ config/permission.php
@@ -7,8 +7,8 @@ return [
     'table_names' => [
         'roles' => 'roles',
         'permissions' => 'permissions',
-        'model_has_permissions' => 'model_has_permissions',
-        'model_has_roles' => 'model_has_roles',
+        'model_has_permissions' => 'user_has_permissions',
+        'model_has_roles' => 'user_has_roles',
         'role_has_permissions' => 'role_has_permissions',
     ],
     'cache_expiration_time' => 60 * 24,
