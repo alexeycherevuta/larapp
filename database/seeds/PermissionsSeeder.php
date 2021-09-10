effe1a20998f4a5e70c98ea9e59d06a381b09d79
Alexey <alexey.cherevuta@devit-team.com>
2021-09-10 20:18:29 +0000

    updated taiwind to 0.2.2.

--- database/seeds/PermissionsSeeder.php
+++ database/seeds/PermissionsSeeder.php
@@ -7,34 +7,16 @@ class PermissionsSeeder extends Seeder
     public function run()
     {
         $permissions = [
-            'admin' => [
-                'create-articles',
-                'edit-articles',
-                'delete-articles',
-                'create-roles',
-                'delete-roles',
-                'create-permissions',
-                'delete-permissions',
-                'set-permission',
-                'set-roles'
-            ],
-            'subscriber' => [
-                'create-articles',
-                'edit-articles',
-                'delete-articles',
-            ]
+            'create-articles',
+            'edit-articles',
+            'delete-articles'
         ];
         $role = Role::findByName('admin');
-        foreach ($permissions['admin'] as $permission){
+        foreach ($permissions as $permission){
             $newPermissions = Permission::create(['name' => $permission]);
-            $this->command->info("[Permission #{$newPermissions->name}] created.");
+            $this->command->info("[Permission #{$newPermissions->id}] Permission {$newPermissions->name} created.");
             $role->givePermissionTo($permission);
-            $this->command->info("[Permission #{$newPermissions->name}] Added to {$role->name} .");
-        }
-        $role = Role::findByName('subscriber');
-        foreach ($permissions['subscriber'] as $permission){
-            $role->givePermissionTo($permission);
-            $this->command->info("[Permission #{$permission}] Added to {$role->name} .");
+            $this->command->info("[Permission #{$newPermissions->id}] Added to {$role->name} .");
         }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -7,15 +7,5 @@ Route::group(['middleware' => 'auth'], function () {
     Route::post('image', 'ImageController@store')->name('image.create');
     Route::prefix('admin')->group(function () {
         Route::resource('/blog', 'PostController');
-        Route::get('/permissions', 'PermissionsController@index')->name('permissions.index');
-        Route::post('/permissions/create', 'PermissionsController@createPermission')->name('permissions.create');
-        Route::get('/permissions/{id}/delete', 'PermissionsController@deletePermission')->name('permissions.delete');
-        Route::post('/permissions/add', 'PermissionsController@addpermission')->name('role.create');
-        Route::post('/permissions/revoke', 'PermissionsController@revokepermission')->name('role.delete');
-        Route::get('/role', 'RoleController@index')->name('role.index');
-        Route::post('/roles/create', 'RoleController@createRole')->name('roles.create');
-        Route::get('/roles/{id}/delete', 'RoleController@deleteRole')->name('roles.delete');
-        Route::get('/roles/{id}/edit', 'RoleController@showEdit')->name('roles.edit');
-        Route::post('/roles/update', 'RoleController@update')->name('roles.update');
     });
 });
